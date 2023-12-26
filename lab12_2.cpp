682306047bbec1da6f5f15205a2c35ad8c2aa200
BeCK8Pi <khuntip1207@gmail.com>
2023-12-26 15:57:24 +0700

    เสดละ

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int, int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x, int y){
+    if(x%y==0){
+        return y;
+    }
+    if(x>=y){
+        return gcd(x%y,y);
+    }
+    if(x<y){
+        return gcd(y,x);
+    }
+    return 69;
 }
